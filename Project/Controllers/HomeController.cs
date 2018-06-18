d041bd023e15b790568e3eec19f7731464e8e0c0
AndriiAleksandrov <37669720+AndriiAleksandrov@users.noreply.github.com>
2018-06-18 15:59:53 +0200

    Registration and Login

--- Project/Controllers/HomeController.cs
+++ Project/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace Project.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
---
+++ Project/Models/Extended/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class UserMetadata
+    {
+        [Display(Name ="First Name")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="First Name required")]
+        public string FirstName { get; set; }
+
+        [Display(Name ="Last Name")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Last Name required")]
+        public string LastName { get; set; }
+
+        [Display(Name ="Email")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Email required")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailID { get; set; }
+
+        [Display(Name ="Date of birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode =true,DataFormatString ="(0:MM-dd-yyyy)")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(6,ErrorMessage ="Minmum 6 characters required")]
+        public string Password { get; set; }
+
+        [Display(Name ="Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Passwoard do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
